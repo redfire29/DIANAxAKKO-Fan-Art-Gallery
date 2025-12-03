04da5914e0ac5ccd11d374dcf65ebd3ccb71c020
DIANAxAKKO <redfire29@gmail.com>
2025-12-03 20:56:00 +0800

    feat: add initial gallery image data

--- app/data/gallery.js
+++ app/data/gallery.js
@@ -2,7 +2,73 @@ export const galleryImages = [
   {
     id: 1,
     url: 'https://pbs.twimg.com/media/G7K40WraAAATXXT?format=jpg&name=large',
-    hashtags: ['うみコレオ'],
+    hashtags: ['うみコレオ', '2025'],
     link: 'https://x.com/redfire299/status/1995870666487386557'
+  },
+  {
+    id: 2,
+    url: 'https://pbs.twimg.com/media/G54Ox8KbEAE2u82?format=jpg&name=4096x4096',
+    hashtags: ['うみコレオ', '2025'],
+    link: 'https://x.com/redfire299/status/1990044424009904314'
+  },
+  {
+    id: 3,
+    url: 'https://pbs.twimg.com/media/G5pFUvYacAQSxoA?format=jpg&name=large',
+    hashtags: ['しゆのアトリエ', '2025'],
+    link: 'https://x.com/redfire299/status/1988985330461208646'
+  },
+  {
+    id: 4,
+    url: 'https://pbs.twimg.com/media/G461skWbMAAgXJi?format=jpg&name=4096x4096',
+    hashtags: ['あか絵るら', '2025'],
+    link: 'https://x.com/redfire299/status/1985724418560311754'
+  },
+  {
+    id: 5,
+    url: 'https://pbs.twimg.com/media/G1W55hcaQAAeOsX?format=jpg&name=4096x4096',
+    hashtags: ['あか絵るら', '2025'],
+    link: 'https://x.com/redfire299/status/1969686496207913164'
+  },
+  {
+    id: 6,
+    url: 'https://pbs.twimg.com/media/G0s-Ju4b0AACAHe?format=jpg&name=large',
+    hashtags: ['うみコレオ', '2025'],
+    link: 'https://x.com/redfire299/status/1966734471949910372'
+  },
+  {
+    id: 7,
+    url: 'https://pbs.twimg.com/media/G0JDcevaAAAIYir?format=jpg&name=4096x4096',
+    hashtags: ['沐藝工坊', '2025'],
+    link: 'https://x.com/redfire299/status/1964207885547708463'
+  },
+  {
+    id: 8,
+    url: 'https://pbs.twimg.com/media/Gz2iLW3bgAATILS?format=jpg&name=4096x4096',
+    hashtags: ['うみコレオ', '2025'],
+    link: 'https://x.com/redfire299/status/1962903450637017174'
+  },
+  {
+    id: 9,
+    url: 'https://pbs.twimg.com/media/GzC2nm0bIAAMWKO?format=jpg&name=4096x4096',
+    hashtags: ['しゆのアトリエ', '2025'],
+    link: 'https://x.com/redfire299/status/1959268357997297735'
+  },
+  {
+    id: 10,
+    url: 'https://pbs.twimg.com/media/GyjFSYqbcAAwQ7Z?format=jpg&name=4096x4096',
+    hashtags: ['うみコレオ', '2025'],
+    link: 'https://x.com/redfire299/status/1957031431714033856'
+  },
+  {
+    id: 11,
+    url: 'https://pbs.twimg.com/media/GyPN4N_asAAmTBR?format=jpg&name=4096x4096',
+    hashtags: ['沐藝工坊', '2025'],
+    link: 'https://x.com/redfire299/status/1955633179827802547'
+  },
+  {
+    id: 12,
+    url: 'https://pbs.twimg.com/media/Gx13nlZbsAIesmR?format=jpg&name=4096x4096',
+    hashtags: ['うみコレオ', '2025'],
+    link: 'https://x.com/redfire299/status/1953849503045575035'
   },
 ];
