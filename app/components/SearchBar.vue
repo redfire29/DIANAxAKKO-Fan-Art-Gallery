f02b9c25fe92b72b2f24054fbccaaf3bdce05114
巧克力 <redfire29@gmail.com>
2025-12-04 09:46:42 +0800

    style: 移除搜尋欄的寬度限制與下邊距。

--- app/components/SearchBar.vue
+++ app/components/SearchBar.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-div(class="w-full max-w-md mx-auto mb-8")
+div(class="w-full mx-auto")
   div(class="relative")
     div(class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none")
       svg(
