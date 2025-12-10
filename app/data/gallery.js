3a3d5c0654bfe53eb6c45d2766a95915ef9f01d1
DIANAxAKKO <redfire29@gmail.com>
2025-12-10 22:26:29 +0800

    feat: Add initial gallery image data to `gallery.js`.

--- app/data/gallery.js
+++ app/data/gallery.js
@@ -1,5 +1,11 @@
 export const galleryImages = [
   {
+    date: '2025/12/10',
+    url: 'https://pbs.twimg.com/media/G70GDxEaMAAB0lr?format=jpg&name=small',
+    hashtags: ['うみコレオ', '2025'],
+    link: 'https://x.com/redfire299/status/1998760840967999667'
+  },
+  {
     date: '2025/12/09',
     url: 'https://pbs.twimg.com/media/G7ud-s2a0AARtxT?format=jpg&name=small',
     hashtags: ['月城様献上品', '2025'],
