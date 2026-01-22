767a5d061b1a2794a8c1a7f037887ea9c379e95c
DIANAxAKKO <redfire29@gmail.com>
2026-01-23 00:36:42 +0800

    Add new gallery entry (2026/01/23)

--- app/data/gallery.js
+++ app/data/gallery.js
@@ -1,5 +1,11 @@
 export const galleryImages = [
   {
+    date: '2026/01/23',
+    url: 'https://pbs.twimg.com/media/G_RmmPjboAA3dbW?format=jpg&name=small',
+    hashtags: ['うみコレオ', '2026'],
+    link: 'https://x.com/redfire299/status/2014352368180297994'
+  },
+  {
     date: '2026/01/22',
     url: 'https://pbs.twimg.com/media/G_MqLUdbMAA8WjQ?format=jpg&name=small',
     hashtags: ['うみコレオ', '2026'],
