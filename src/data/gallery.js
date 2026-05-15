ed031f1d8fdeb990024dca05dad5d6056f532341
DIANAxAKKO <redfire29@gmail.com>
2026-05-16 00:15:22 +0800

    feat(gallery): 新增 2026/05/16 的粉絲藝術圖
    新增一張由 redfire299 創作的粉絲藝術圖。
    
    AI Model: Antigravity (Gemini 3 Flash)

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,5 +1,11 @@
 export const galleryImages = [
   {
+    date: '2026/05/16',
+    url: 'https://pbs.twimg.com/media/HIX2gufa4AAx8Wm?format=jpg&name=small',
+    hashtags: ['うみコレオ', '2026'],
+    link: 'https://x.com/redfire299/status/2055319975771201696'
+  },
+  {
     date: '2026/05/10',
     url: 'https://pbs.twimg.com/media/HH45sO9b0AAPVQA?format=jpg&name=small',
     hashtags: ['うみコレオ', '2026'],
