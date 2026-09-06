50f3244406507f044265077d28e6244aef80f9bb
DIANAxAKKO <redfire29@gmail.com>
2026-09-06 20:46:49 +0800

    feat: add fan art collection data to gallery.js

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,5 +1,11 @@
 export const galleryImages = [
   {
+    date: '2026/09/06',
+    url: 'https://pbs.twimg.com/media/HRiLsCmb0AAxNGK?format=jpg&name=small',
+    hashtags: ['うみコレオ', '2026'],
+    link: 'https://x.com/redfire299/status/2096579949746692328'
+  },
+  {
     date: '2026/09/04',
     url: 'https://pbs.twimg.com/media/HRYOvkkbwAArFzC?format=jpg&name=small',
     hashtags: ['うみコレオ', '2026'],
